0028ab003b2afab321280c69f147c536f4af338b
cakeonfire <frank.pos.ziel@gmail.com>
2025-10-15 12:42:14 +0200

    Update z1 for both C and C++ versions

--- z1.cpp
+++ z1.cpp
@@ -4,12 +4,22 @@
 
 int main(void) {
 
+    // C version
     printf("\t\tXXXXXX\t\tXXXXXX\n");
     printf("\t\tX     \t\tX     X\n");
     printf("\t\tXXXXX \t\tXXXXXX\n");
     printf("\t\tX     \t\tX\n");
     printf("\t\tX     \t\tX\n");
     printf("\t\tX     \t\tX\n");
+    printf("\n");
+
+    // C++ version
+    std::cout << "\t\tXXXXXX\t\tXXXXXX\n";
+    std::cout << "\t\tX     \t\tX     X\n";
+    std::cout << "\t\tXXXXX \t\tXXXXXX\n";
+    std::cout << "\t\tX     \t\tX\n";
+    std::cout << "\t\tX     \t\tX\n";
+    std::cout << "\t\tX     \t\tX\n";
 
     return 0;
 }
