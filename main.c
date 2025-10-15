b38811ecd820ec345a90182decc5ef3cf07d27f5
cakeonfire <frank.pos.ziel@gmail.com>
2025-10-15 12:36:14 +0200

    - add compile script (make) - task 1: z1.c

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int main(void) {
+    char str[256];
+    char *text1 = "line 1! ";
+    char *text2 = "line 2! ";
+    char *text3 = "line 3! ";
 
-    printf("Hello, World!");
+    strcpy(str, text1);
+    strcat(str, text2);
+    strcat(str, text3);
+
+    //printf("Hello, World!");
+
+    printf("%s\n", str);
 
     return 0;
 }
---
+++ z1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+
+int main(void) {
+
+    printf("\t\tXXXXXX\t\tXXXXXX\n");
+    printf("\t\tX     \t\tX     X\n");
+    printf("\t\tXXXXX \t\tXXXXXX\n");
+    printf("\t\tX     \t\tX\n");
+    printf("\t\tX     \t\tX\n");
+    printf("\t\tX     \t\tX\n");
+
+    return 0;
+}
